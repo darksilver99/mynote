8d65084e3f0cc7fab23044a1b631f6e033dcbbbd
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-04 02:40:18 +0000

    Updating to latest FlutterFlow output.

--- lib/home_page/home_page_model.dart
+++ lib/home_page/home_page_model.dart
@@ -6,6 +6,7 @@ import '/flutter_flow/flutter_flow_widgets.dart';
 import 'dart:async';
 import 'home_page_widget.dart' show HomePageWidget;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
--- lib/home_page/home_page_widget.dart
+++ lib/home_page/home_page_widget.dart
@@ -5,6 +5,7 @@ import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'home_page_model.dart';
@@ -26,6 +27,12 @@ class _HomePageWidgetState extends State<HomePageWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => HomePageModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      setState(() => _model.requestCompleter = null);
+      await _model.waitForRequestCompleted();
+    });
   }
 
   @override
